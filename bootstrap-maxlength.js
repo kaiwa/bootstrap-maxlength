fac641d293f293852ad4ed61f6e0150a985a3535
Kai W <kaiwatermann@gmail.com>
2012-11-30 12:12:24 +0100

    Initial commit

---
+++ bootstrap-maxlength.js
@@ -0,0 +1,27 @@
+!function ($) {
+  $.fn.maxlength = function() {
+    $(this).each(function() {
+      var max = $(this).attr('maxlength');
+      var val = $(this).attr('value');
+      var cur = 0;
+      if(val)
+        cur = val.length;
+      var left = max - cur;
+
+      $(this).parent().addClass('input-append');
+      $(this).after("<span class='add-on maxlength'>" + left.toString() + "</span>");
+
+      $(this).bind('input', function(e) {
+        var max = $(this).attr('maxlength');
+        var val = $(this).attr('value');
+        var cur = 0;
+        if(val)
+          cur = val.length;
+        var left = max-cur;
+        $(this).next(".maxlength").text(left.toString());
+        return this;
+      });
+    });
+    return this;
+  };
+}(window.jQuery);
