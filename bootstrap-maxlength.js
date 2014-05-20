d8a9d7b00ea4b829e1b7185574a73abe2953f3cb
Maximilien Cuony <maximilien@theglu.org>
2014-05-20 15:22:07 +0200

    Bootstrap 3 compatibilty

--- bootstrap-maxlength.js
+++ bootstrap-maxlength.js
@@ -4,27 +4,27 @@
       var max = $(this).attr('maxlength');
       var val = $(this).attr('value');
       var cur = 0;
-      
+
       if (val) {
           cur = val.length;
       }
 
       var left = max - cur;
 
-      $(this).parent().addClass('input-append');
-      $(this).after("<span class='add-on maxlength'>" + left.toString() + "</span>");
+      $(this).parent().addClass('input-group');
+      $(this).after("<span class='input-group-addon maxlength'>" + left.toString() + "</span>");
 
       $(this).bind('input', function(e) {
         var max = $(this).attr('maxlength');
         var val = $(this).attr('value');
         var cur = 0;
-        
+
         if (val) {
           cur = val.length;
         }
-        
+
         var left = max-cur;
-        
+
         $(this).next(".maxlength").text(left.toString());
 
         return this;
